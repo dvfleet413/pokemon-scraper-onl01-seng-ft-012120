30e4bc092d9fe497b2ff1897319be60832d15ca2
David Van Fleet <dvfleet413@gmail.com>
2020-02-20 19:15:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,20 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id 
   
+  def initialize(name:, type:, db:, id: nil)
+    @name = name 
+    @type = type
+    @db = db 
+    @id = id
+  end
   
+  def save
+    
+  end 
   
-  
+  def find(id)
+    
+  end 
   
 end
